92ad8b7a72e6d863b26b8ea515d6eb680aa343d3
Mario Salinas <mario_1605@live.com.mx>
2020-06-04 06:25:56 -0500

    feature/basic struture of project

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const config = require('../config')
+const user = require('./components/user/network')
+
+const app = express()
+
+app.use('/api/user', user)
+
+app.listen(config.api.port, () => {
+    console.log(`Server is listening at http://localhost:${config.api.port}`);
+})
